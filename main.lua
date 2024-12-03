8bd19d5ef2f86c0389e33b61778617c9e28ceb82
CanineData <90639761+CanineData@users.noreply.github.com>
2024-12-03 12:49:41 +0000

    Test

--- main.lua
+++ main.lua
@@ -1,7 +1,9 @@
 local fileURLBase = "https://raw.githubusercontent.com/CanineData/cc-survival-world/refs/heads/master/"
 
-term.setCursorPos(0, 0)
-term.setBackgroundColor(256)
+term.setBackgroundColor(colors.blue)
 term.clear()
+term.setCursorPos(0, 0)
+
+term.setTextColor(colors.white)
 
 print('Hello World')
