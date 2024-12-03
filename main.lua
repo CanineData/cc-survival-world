5743f5c02d2e434e33d125e5faf4fa62c17463fb
CanineData <90639761+CanineData@users.noreply.github.com>
2024-12-03 12:44:56 +0000

    update

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 local fileURLBase = "https://raw.githubusercontent.com/CanineData/cc-survival-world/refs/heads/master/"
 
-term.clear()
 term.setCursorPos(0, 0)
 term.setBackgroundColor(256)
+term.clear()
